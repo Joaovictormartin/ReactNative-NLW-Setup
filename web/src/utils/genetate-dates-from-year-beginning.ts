275723a926ac96f092d6d5cc67f88d64f9d7b44d
Joao Victor <joaovictordeveloper@outlook.com>
2023-01-19 11:50:47 -0300

    feat: função para gerar os dias do ano

---
+++ web/src/utils/genetate-dates-from-year-beginning.ts
@@ -0,0 +1,16 @@
+import dayjs from "dayjs";
+
+export const genetateDatesFromYearBeginning = () => {
+  const firstDayTheYear = dayjs().startOf("year");
+  const today = new Date();
+
+  const dates = [];
+  let compareDate = firstDayTheYear;
+
+  while (compareDate.isBefore(today)) {
+    dates.push(compareDate.toDate());
+    compareDate = compareDate.add(1, "day");
+  }
+
+  return dates;
+};
